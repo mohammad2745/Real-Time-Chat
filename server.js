89e32ba9061cc5dbb96df085dc95b2308d3b7a8c
Sultan <sultan@bdjobs.local>
2021-11-21 12:49:46 +0600

    Connection with Socket and view messages through socket in console

--- server.js
+++ server.js
@@ -12,9 +12,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', socket => {
-    console.log('New WS Connection.....');
-
+    // Welcome current user
     socket.emit('message', 'Welcome to ChatCord');
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when a client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    // Listen for chat message
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
